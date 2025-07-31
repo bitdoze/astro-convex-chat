0a9b193db4f83780d8afe0ac6494f6a3f2394a5f
Dragos Balota <dragos.balota@oracle.com>
2025-07-31 15:38:27 +0300

    added things

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
